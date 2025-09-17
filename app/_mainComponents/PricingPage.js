b0a5643122e5cde90c9ed31f57927c011a88dbe9
subzero1221 <158075799+subzero1221@users.noreply.github.com>
2025-09-17 22:05:20 +0400

    Update PricingPage.js

--- app/_mainComponents/PricingPage.js
+++ app/_mainComponents/PricingPage.js
@@ -15,7 +15,7 @@ function t(key, lang) {
 export default function PricingPage() {
   const language = useUserStore((state) => state.language);
   const [loadingPlan, setLoadingPlan] = useState(null);
-  const {updateTokens} = userStore(); 
+  const {updateTokens} = useUserStore(); 
   
   const plans = [
     {
